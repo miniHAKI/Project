39aa030fdb6e986afd6df32fe68f36a078133aa4
HAKI <hk647773@gmail.com>
2025-01-27 15:10:49 +0500

    navbar.jsx issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { LuWalletCards } from "react-icons/lu";
 import { RxCrossCircled } from "react-icons/rx";
-import { Link, Links } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [IsLogin, setIsLogin] = useState(false)
@@ -31,7 +31,6 @@ const Navbar = () => {
             {IsLogin ? <li className="cursor-pointer text-lg hover:text-gray-500 hover:underline">Wel Come User</li> : <li className="cursor-pointer text-lg hover:text-gray-500 hover:underline"><Link to={"/signup"}> Signup</Link></li>}
           </ul>
         </div>
-
         <div className="right-section flex items-center gap-4">
           <div className="hidden md:flex items-center gap-2 bg-gray-200 text-black px-4 py-2 rounded-md">
             <input
